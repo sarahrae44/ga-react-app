ac70b88f4f93843bda7e0f9e826343d3f170a53a
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-11-15 11:32:16 -0700

    APIs added

--- blog-post/src/Home.js
+++ blog-post/src/Home.js
@@ -1,14 +1,39 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shakeSpeare: ""
+    }
+  }
+  componentDidMount() {
+    var base = this
+    let poemApi = 'http://ShakeItSpeare.com/api/poem';
+    fetch(poemApi)
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        base.setState({ shakeSpeare: json.poem });
+      }).catch((ex) => {
+        console.log('An error occured while parsing!', ex);
+      })
+  }
+  render() {
+    let poetry = this.state.shakeSpeare;
+    if (this.state.shakeSpeare) {
+      return (
+        <div>
+          <h1>My favorite Shakespeare poem:</h1>
+          {poetry}
+        </div>
+      )
+    }
     return (
       <div>
-        <h1>Thanks for visiting my awesome website!</h1>
-        <p>Here is a joke I really like:</p>
-          <img src="https://i.redd.it/o33ryb4zfc6z.jpg" alt="It turns out computers are just flattened rocks with lightning in them" width="300px" />
-
-      </div>
+        <h1>My favorite Shakespeare poem:</h1>
+        Loading...
+      </div>  
     )
   }
 }
